47d8126e73753988ca0e9e8334bc66513056935d
Sano <sano@qoncept.co.jp>
2019-04-02 19:28:26 +0900

    Add LibraryTest

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "sbt-library-test"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+publishTo := Some(Resolver.file("library-test", new File("../product")))
---
+++ src/main/scala/info/biacco42/lib/librarytest/LibraryTest.scala
@@ -0,0 +1,11 @@
+package info.biacco42.lib.librarytest
+
+/**
+  * Created by Biacco42 on 2019-04-02.
+  */
+class LibraryTest {
+}
+
+object LibraryTest {
+  def greeting: String = "Hello SBT library!"
+}
